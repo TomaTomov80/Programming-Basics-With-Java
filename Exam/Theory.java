a52d2066c583d18dd7ebb8508cc762e07fd61515
TomaTomov80 <tomatomov80@gmail.com>
2022-04-04 13:21:40 +0300

    Some files deleted.

--- Exam/Theory.java
+++
@@ -1,9 +0,0 @@
-package Exam;
-
-import java.util.Scanner;
-
-public class Theory {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
--- NestedLoops/Exercises/test.java
+++
@@ -1,4 +0,0 @@
-package NestedLoops.Exercises;
-
-public class test {
-}
